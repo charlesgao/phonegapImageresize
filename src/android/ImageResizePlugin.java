42046aa708b47d4b232cde818e75362501d783d5
Charles <charlesgao12@gmail.com>
2014-01-05 22:21:24 +0800

    Update ImageResizePlugin.java
    update cordova lib import to phonegap 3.0

--- src/android/ImageResizePlugin.java
+++ src/android/ImageResizePlugin.java
@@ -20,8 +20,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
